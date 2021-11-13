112a4c08d74920d9a9f94ad2640cb71fe97727b1
Hamza Ali <hamali1708@gmail.com>
2021-11-12 23:22:46 -0600

    add the Home and created a nav bar

---
+++ app/src/main/java/com/codepath/museshare/adapters/SongsAdapter.java
@@ -0,0 +1,45 @@
+package com.codepath.museshare.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.museshare.R;
+
+public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder>{
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvAuthor;
+        ImageView ivAlbum;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvAuthor = itemView.findViewById(R.id.tvAuthor);
+            ivAlbum = itemView.findViewById(R.id.ivAlbum);
+        }
+    }
+}
