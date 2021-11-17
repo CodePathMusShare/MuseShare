b468a8ede4d6cfe7b22fe092c9c55183de04ad69
Hamza Ali <hamali1708@gmail.com>
2021-11-16 22:56:04 -0600

    finish setting up the nav bar and the fragments

--- app/src/main/java/com/codepath/museshare/MainActivity.java
+++ app/src/main/java/com/codepath/museshare/MainActivity.java
@@ -1,14 +1,63 @@
 package com.codepath.museshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.codepath.museshare.fragments.HomeFragment;
+import com.codepath.museshare.fragments.MessageFragment;
+import com.codepath.museshare.fragments.PlayListFragment;
+import com.codepath.museshare.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) { // TO DO: Create fragment files
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_playlist:
+                        Toast.makeText(MainActivity.this, "PlayList!", Toast.LENGTH_SHORT).show();
+                        fragment = new PlayListFragment();
+                        break;
+                    case R.id.action_message:
+                        Toast.makeText(MainActivity.this, "Message!", Toast.LENGTH_SHORT).show();
+                        fragment = new MessageFragment();
+                        break;
+                    case R.id.action_settings:
+                    default:
+                        Toast.makeText(MainActivity.this, "Settings!", Toast.LENGTH_SHORT).show();
+                        fragment = new SettingsFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+                //fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit();
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
     }
 }
