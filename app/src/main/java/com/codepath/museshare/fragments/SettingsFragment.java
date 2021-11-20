b8896e5203263402cec4e3d1d03efaaed2ebe122
ifsfiq <faruiq@gmail.com>
2021-11-19 23:41:20 -0600

    finished log out button

--- app/src/main/java/com/codepath/museshare/fragments/SettingsFragment.java
+++ app/src/main/java/com/codepath/museshare/fragments/SettingsFragment.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.codepath.museshare.R;
 
@@ -18,9 +19,10 @@ public class SettingsFragment extends Fragment {
     public static final String TAG = "HomeFragment";
     private RecyclerView rvSongs;
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
