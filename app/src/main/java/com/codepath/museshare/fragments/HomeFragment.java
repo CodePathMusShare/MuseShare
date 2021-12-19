e7d3f271278c30ecc69936d8ebdd8142a92389d8
Hamza Ali <hamali1708@gmail.com>
2021-12-19 13:19:23 -0600

    updating

--- app/src/main/java/com/codepath/museshare/fragments/HomeFragment.java
+++ app/src/main/java/com/codepath/museshare/fragments/HomeFragment.java
@@ -21,7 +21,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        return inflater.inflate(R.layout.home_example, container, false);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
